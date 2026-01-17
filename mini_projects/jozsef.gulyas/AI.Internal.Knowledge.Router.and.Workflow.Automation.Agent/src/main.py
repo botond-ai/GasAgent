9958bb73b457da970460653f0e2508662fce5770
Joe <mr.gulyas.jozsef.ev@gmail.com>
2026-01-17 14:54:00 +0100

    Application structure foundations

---
+++ mini_projects/jozsef.gulyas/AI.Internal.Knowledge.Router.and.Workflow.Automation.Agent/src/main.py
@@ -0,0 +1,8 @@
+from presentation.cli_interface import CliInterface
+
+def main():
+    print("Hello from ai-internal-knowledge-router-and-workflow-automation-agent!")
+
+
+if __name__ == "__main__":
+    main()
